d7408090b99c8bd635886d253ef1289f71d67001
weiguo <weiguo@ualberta.ca>
2025-01-25 22:01:39 -0500

    Minor changes.

--- main.py
+++ main.py
@@ -17,9 +17,6 @@ import zipfile
 client = OpenAI(api_key="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")
 
 
-# Set OpenAI API key
-
-
 def welcome():
     print("""
     ================================================================================
